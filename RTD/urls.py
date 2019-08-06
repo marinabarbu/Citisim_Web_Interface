3f1cceb0c6184252d76a922161974d37b156708c
Tehnic <barbumarina.madalina97@gmail.com>
2019-08-06 13:50:44 +0300

    Added navbar with links to home page, to a ChartJS and also added reference for details for each data and reference for each source

--- RTD/urls.py
+++ RTD/urls.py
@@ -5,5 +5,5 @@ urlpatterns = [
     url(r'^$', views.index, name='index'),
     url(r'^(?P<e_idd>[0-9]+)/$', views.detail, name='detail'),
     url(r'^(?P<e_source>[a-z0-9A-Z]+)/$', views.data_source, name='data_source'),
-    url(r'^data/$', views.get_data, name='get_data')
+    url(r'^get_data/$', views.get_data, name='get_data')
 ]
--- RTD/views.py
+++ RTD/views.py
@@ -25,14 +25,8 @@ def data_source(request, e_source):
     return render(request, 'RTD/data_source.html', {'all_energy' : all_energy})
 
 def get_data(request):
-
-    data = {
-        'male_data': [41, 26, 57, 47, 49, 40, 67, 68, 24, 26],
-        'female_data': [62, 39, 67, 33, 58, 67, 50, 48, 21, 30],
-        'label_data': ['13-17', '18-24', '25-34', '34-44', '45-54', '55-64'],
-    }
-
-    return render(request, 'RTD/data.html', data)
+    all_energy = Energy.objects.all()
+    return render(request, 'RTD/get_data.html', {'all_energy' : all_energy})
 
 
 
